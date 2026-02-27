c3e50ad42021c58ecaf528bdc132febb68315a25
Alexander Lavrentyev <alexander.lavrentyev@sirenltd.com>
2026-02-27 21:32:45 +0100

    Add speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { inject } from "@vercel/analytics";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import App from "./App";
 
@@ -9,5 +10,6 @@ inject();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
